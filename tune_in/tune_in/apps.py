989fdd6af041f73bd1fe4d3bcf4acfda87148684
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-06-26 16:44:03 -0700

    scrapped

--- tune_in/tune_in/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TuneInConfig(AppConfig):
-    name = 'tune_in'
--- tune_in/tune_in/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'tune_in'
-urlpatterns = [
-    path('', views.index, name='index'),
-]
