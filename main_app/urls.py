8383212186981016e8646e61c7683b0fcb6a95a6
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-07-10 16:41:21 -0700

    fixed url page

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('login/', views.login_page, name='login_page'),
     path('sign_up/', views.sign_up, name='sign_up'),
-    # path('', views.detail, name='detail'),
 ]
