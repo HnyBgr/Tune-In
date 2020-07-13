b85a26b50330e320b8fcf3a8d4bb2d18dc7b58b3
Troy-Fitz <63209242+Troy-Fitz@users.noreply.github.com>
2020-07-12 17:45:04 -0700

    added login page; updated views, urls, index

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,8 @@ app_name = 'main_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('motion/', views.motion, name='motion'),
+    path('login/', views.login, name='login'),
     path('sign_up/', views.sign_up, name='sign_up'),
+    # test page for animation
+    path('motion/', views.motion, name='motion'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from .models import MusicInfo
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 
 def index(request):
     this_day = MusicInfo.objects.order_by('day')
@@ -22,6 +25,25 @@ def index(request):
     }
     return render(request, 'main_app/index.html', data)
 
+
+
+def login(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, usename=username, password=password)
+        
+        login(request, user)
+
+        if 'next' in request.GET:
+            return HttpResponseRedirect(request.GET['next'])
+
+        return HttpResponseRedirect(reverse('main_app/index.html'))
+    return render(request, 'main_app/login.html')
+
+
 def sign_up(request):
     if request.method == 'POST':
 
@@ -36,7 +58,7 @@ def sign_up(request):
         user = User.objects.create_user(username, email, password)
         return HttpResponseRedirect(reverse('main_app/index.html'))
     
-    return render(request, 'main_app/sign_up.html' )
+    return render(request, 'main_app/sign_up.html')
 
 def motion(request):
     return render(request, 'main_app/motion.html')
